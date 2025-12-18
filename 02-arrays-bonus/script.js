5b998881304d8ce3fd223d24a9a6852b1736c50f
micheleferri01 <micheleferri095@gmail.com>
2025-12-18 15:56:02 +0100

    soluzione punto 3 (02-arrays-bonus)

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -40,6 +40,12 @@ for(let i = 0; i < teachers.length; i++){
 console.table(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+for(let i = 0; i < teachers.length; i++){
+  if(teachers[i] === 'Ed'){
+    teachers.splice(i,1);
+  }
+}
+console.table(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
