0300fca994924318e8ffffb3e86d2115b9ee7018
micheleferri01 <micheleferri095@gmail.com>
2025-12-18 16:04:59 +0100

    soluzione punto 4 (02-arrays-bonus)

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -49,7 +49,13 @@ console.table(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+let isFabioPresent = false;
+for(let i = 0; i < teachers.length; i++){
+  if(teachers[i] === 'Fabio'){
+    isFabioPresent = true;
+  }
+}
+console.log('Fabio è prensente nella lista:', isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
