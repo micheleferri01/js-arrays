83a1b6e6d2ddd58cfb11c4fa698e39e03a4a1c5a
micheleferri01 <micheleferri095@gmail.com>
2025-12-18 15:13:58 +0100

    soluzione punto 8 (01-arrays)

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -47,4 +47,8 @@ for (let i = 0; i < teachers.length; i++) {
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+let isTeachersEmpty = false;
+if (teachers.length === 0){
+isTeachersEmpty = true;
+}
+console.log("La lista teachers è vuota?",isTeachersEmpty);
