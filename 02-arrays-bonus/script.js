4de73dfbd41f55272e53f7efde93ba9b327c1989
micheleferri01 <micheleferri095@gmail.com>
2025-12-18 15:36:29 +0100

    soluzione punto 1 (02-arrays-bonus)

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -8,9 +8,26 @@ const teachers = [
   'Luca'
 ]; // NON MODIFICARE QUESTA VARIABILE
 
+console.table(teachers);
+
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = [];
+for(let i = teachers.length - 1; i >= 0; i--){
+reversedTeachers.push(teachers[i]);
+}
+console.table(reversedTeachers);
+
+// !punto 1 soluzione 2
+const reversedTeachersSolution2 = [];
+for( let i = 0; i < teachers.length; i++){
+  reversedTeachersSolution2.unshift(teachers[i]);
+}
+console.table(reversedTeachersSolution2);
+
+// !punto 1 soluzione 3
+const reversedTeachersSolution3 = teachers.reverse();
+console.table(reversedTeachersSolution3);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
